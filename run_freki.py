f4d8e3d9df0908192f9a138aa823dda85c3f4078
Michael Wayne Goodman <goodman.m.w@gmail.com>
2016-11-16 16:46:57 -0800

    Fix #5 - add gzip output option; deindent document
    This primarily adds gzip output support, but it also removes the option to deindent blocks, instead defaulting to deindenting the entire document as one block. This will preserve indentation within the document, but will remove unnecessary leading spaces from most lines.
    
    This commit may break some unit tests, though.

--- run_freki.py
+++ run_freki.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
 import os
-from os import path
-from collections import defaultdict, Counter
+# from collections import defaultdict, Counter
+import gzip
 import argparse
 import logging
-from builtins import open
-from past.builtins import unicode
-
-import sys
 
 from freki.readers import tetml, pdfminer
 from freki.serialize import FrekiBlock, FrekiLine, FrekiDoc
@@ -20,93 +16,107 @@ readers = {
 
 def run(args):
     reader = readers[args.format](args.infile)
-    doc_id = path.splitext(path.basename(args.infile))[0]
-    line_no = 1
 
-    # Initialize the freki document
-    fd = FrekiDoc()
+    doc_id = _doc_id_from_path(args.infile)
 
-    # a) Is the outfile argument file-like?
-    #    if yes, use that stream directly.
-    if hasattr(args.outfile, 'write'):
-        outfile = args.outfile
-
-    # b) Is there an outfile path specified?
-    #    if yes, open that path
-    elif args.outfile is not None:
+    if args.outfile is None or hasattr(args.outfile, 'write'):
+        if args.gzip:
+            raise Exception('Cannot gzip to an open stream.')
+        analyze(reader, args.outfile, doc_id, args.block)
+    else:
+        if args.gzip:
+            openfile = gzip.open
+            if not args.outfile.endswith('.gz'):
+                args.outfile += '.gz'
+        else:
+            openfile = open
         dirs = os.path.dirname(args.outfile)
         if dirs:
-            try:
-                os.makedirs(dirs)
-            except OSError as oe:
-                pass
-        outfile = open(args.outfile, 'w', encoding='utf-8')
+            os.makedirs(dirs, exist_ok=True)
+        with openfile(args.outfile, 'wb') as outfile:
+            analyze(reader, outfile, doc_id, args.block)
 
-    # Otherwise,
-    else:
-        outfile = sys.stdout
+def analyze(reader, outfile, doc_id, block_coefficient):
+    # Initialize the freki document
+    fd = FrekiDoc()
+    line_no = 1
+
+    # find minimum left-coordinate if available
+    l_margin = [t.llx for p in reader.pages() for t in p.tokens]
+    l_margin = min(l_margin) if l_margin else 0.0
 
     for page in reader.pages():
-        for blk in reader.blocks(page, coefficient=args.block):
-            fb = FrekiBlock(doc_id=doc_id,
-                            page=page.id,
-                            block_id=blk.id,
-                            bbox='{},{},{},{}'.format(blk.llx,blk.lly,blk.urx,blk.ury),
-                            doc=fd)
+        for blk in reader.blocks(page, coefficient=block_coefficient):
+            fb = FrekiBlock(
+                doc_id=doc_id,
+                page=page.id,
+                block_id=blk.id,
+                bbox='{},{},{},{}'.format(blk.llx, blk.lly, blk.urx, blk.ury),
+                doc=fd
+            )
 
-            for i, line in enumerate(respace(blk, args.deindent_blocks)):
-                fonts = ','.join(sorted(set(['{}-{}'.format(t.font, t.size) for t in blk.lines[i].tokens])))
-                fl = FrekiLine(line,
-                               line=line_no+i,
-                               fonts=fonts)
+            for i, line in enumerate(respace(blk, xoffset=(l_margin * -1))):
+                fonts = ','.join(
+                    sorted(set(['{}-{}'.format(t.font, t.size)
+                                for t in blk.lines[i].tokens]))
+                )
+                fl = FrekiLine(
+                    line,
+                    line=line_no+i,
+                    fonts=fonts
+                )
                 fb.add_line(fl)
 
             fd.add_block(fb)
 
             line_no += len(blk.lines)
+            #print('\n'.join(respace(blk)))
+            # outfile.write(str(fb)+'\n\n')
 
-    outfile.write(unicode(fd))
-
-
-def tabularize(block):
-    # more aggressively try to pigeonhole aligned tokens
-    def approx(n):
-        return round(n/2, 0)
-    tab_indices = sorted(block.tabular_lines())
-    lines = [list(line.tokens) for line in block.lines]
-    colocs = Counter(approx(t.llx) for idx in tab_indices for t in lines[idx])
-    llx_map = {llx: defaultdict(list) for llx in colocs}
-    for i, line in enumerate(lines):
-        if i not in tab_indices:
-            continue
-        cur_llx = 0
-        last_x = 0
-        for tok in line:
-            llx = approx(tok.llx)
-            if colocs[llx] > 1 or (llx - last_x) > (tok.width):
-                cur_llx = llx  # aligned or has significant space
-            llx_map[cur_llx][i].append(tok)
-            last_x = approx(tok.urx)
-    tablines = defaultdict(list)
-    for _, row_dict in sorted(llx_map.items()):
-        if len(row_dict) == 0:
-            continue
-        toks = [''.join(t.text for t in row_dict.get(i, []))
-                for i in tab_indices]
-        maxlen = max(len(t) for t in toks)
-        for i, tok in enumerate(toks):
-            tablines[i].append(tok.ljust(maxlen))
-    for i, line in enumerate(lines):
-        if i in tab_indices:
-            yield ' '.join(tablines[i])
-        else:
-            yield ' '.join(t.text for t in line)
+            # lines = reader.group_lines()
+            # respace(lines)
+    if outfile is None:
+        print(str(fd))
+    else:
+        outfile.write(str(fd).encode('utf-8'))
 
 
-def respace(block, deindent=False):
-    t = list((t.llx for line in block.lines for t in line.tokens))
-    l_margin = min(t) if (deindent and t) else 0
+# def tabularize(block):
+#     # more aggressively try to pigeonhole aligned tokens
+#     def approx(n):
+#         return round(n/2, 0)
+#     tab_indices = sorted(block.tabular_lines())
+#     lines = [list(line.tokens) for line in block.lines]
+#     colocs = Counter(approx(t.llx) for idx in tab_indices for t in lines[idx])
+#     llx_map = {llx: defaultdict(list) for llx in colocs}
+#     for i, line in enumerate(lines):
+#         if i not in tab_indices:
+#             continue
+#         cur_llx = 0
+#         last_x = 0
+#         for tok in line:
+#             llx = approx(tok.llx)
+#             if colocs[llx] > 1 or (llx - last_x) > (tok.width):
+#                 cur_llx = llx  # aligned or has significant space
+#             llx_map[cur_llx][i].append(tok)
+#             last_x = approx(tok.urx)
+#     tablines = defaultdict(list)
+#     for _, row_dict in sorted(llx_map.items()):
+#         if len(row_dict) == 0:
+#             continue
+#         toks = [''.join(t.text for t in row_dict.get(i, []))
+#                 for i in tab_indices]
+#         maxlen = max(len(t) for t in toks)
+#         for i, tok in enumerate(toks):
+#             tablines[i].append(tok.ljust(maxlen))
+#     for i, line in enumerate(lines):
+#         if i in tab_indices:
+#             yield ' '.join(tablines[i])
+#         else:
+#             yield ' '.join(t.text for t in line)
 
+
+def respace(block, xoffset=0.0):
     # -------------------------------------------
     # We want to calculate the average character
     # width for a block. The numerator is the sum of
@@ -132,7 +142,7 @@ def respace(block, deindent=False):
         last_x = 0
         cur_line = []
         for t in line.tokens:
-            llx = t.llx - l_margin
+            llx = t.llx + xoffset
             if (llx - last_x) < min_dx:
                 dx = 0
             elif char_dx == 0:
@@ -142,33 +152,41 @@ def respace(block, deindent=False):
                 dx = int(((llx - last_x) / char_dx) + 0.5) or 1
             cur_line.append(' ' * dx)
             cur_line.append(t.text if t.text is not None else '')
-            last_x = t.urx - l_margin
+            last_x = t.urx + xoffset
         lines.append(''.join(cur_line))
         prev = {}
     return lines
+
+def _doc_id_from_path(path):
+    bn = os.path.basename(path)
+    if bn.endswith('.gz'):
+        bn = bn[:-3]
+    return os.path.splitext(bn)[0]
 
 def main(arglist=None):
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description="Find IGTs in text extracted from a PDF",
         epilog='examples:\n'
-            '    freki.py --format tetml infile.xml > outfile.txt'
+               '    freki.py --format tetml infile.xml > outfile.txt'
     )
-    parser.add_argument('-v', '--verbose',
+    parser.add_argument(
+        '-v', '--verbose',
         action='count', dest='verbosity', default=2,
         help='increase the verbosity (can be repeated: -vvv)'
     )
     parser.add_argument(
         '-f', '--format',
-        choices=('tetml','pdfminer'), default='tetml'
+        choices=('tetml', 'pdfminer'), default='tetml'
     )
-    parser.add_argument('-b', '--block',
+    parser.add_argument(
+        '-b', '--block',
         type=float, default=0.0,
         help='block grouping multiplier'
     )
-    parser.add_argument('--deindent-blocks',
-        action='store_true',
-        help='remove consistent leading space in block lines'
+    parser.add_argument(
+        '-z', '--gzip',
+        action='store_true', help='gzip output file'
     )
     parser.add_argument('infile')
     parser.add_argument('outfile')
